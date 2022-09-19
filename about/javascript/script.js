a729acb681ba7882ef8636c373f5bf3a6468edf2
Troy Nicholls <nicholls_troy@outlook.com>
2022-09-19 11:09:35 -0400

    Build website

---
+++ about/javascript/script.js
@@ -0,0 +1,24 @@
+function tpMenu() {
+    const link = document.querySelector('.links');
+
+    if (link.style.display === "none") {
+        link.style.display = "block";
+    }
+    else {
+        link.style.display = "none";
+    }
+}
+
+// Variable
+const accordion = document.getElementsByClassName('content')
+
+//This allows us to loop through all of our classes under the accordion class
+for(i=0; i < accordion.length; i++) {
+    //We then add a click event to each class 
+    accordion[i].addEventListener('click', function () {
+        //The toggle will add the active and remove the active when clicking
+        this.classList.toggle('active')
+    })
+}
+
+
---
+++ home/javascript/script.js
@@ -0,0 +1,10 @@
+function tpMenu() {
+    const link = document.querySelector('.links');
+
+    if (link.style.display === "none") {
+        link.style.display = "block";
+    }
+    else {
+        link.style.display = "none";
+    }
+}
